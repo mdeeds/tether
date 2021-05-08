6e458422e49b6d857b4a162e95d3592b4556c887
Matt Deeds <mdeeds@gmail.com>
2021-05-08 11:29:40 -0700

    Text area so we have selection offset.

--- ts/sharedBox.ts
+++ ts/sharedBox.ts
@@ -3,31 +3,35 @@ import { PeerGroupInterface } from "./peerGroupInterface";
 
 export class SharedBox {
   private lockedText: LockedText;
-  readonly div: HTMLDivElement;
+  readonly div: HTMLTextAreaElement;
   private lastContent: string;
 
   constructor(peerGroup: PeerGroupInterface,
     container: HTMLDivElement | HTMLBodyElement) {
     this.lockedText = new LockedText(peerGroup);
-    this.div = document.createElement('div');
+    this.div = document.createElement('textarea');
     this.div.classList.add('testBox');
     this.div.contentEditable = 'true';
     this.div.spellcheck = false;
     container.appendChild(this.div);
 
     this.div.addEventListener('keyup', async (ev: KeyboardEvent) => {
-      if (this.lastContent != this.div.innerText) {
+      if (this.lastContent != this.div.value) {
         if (!this.lockedText.hasLock()) {
-          this.div.innerHTML =
+          this.div.value =
             await this.lockedText.takeLock();
         } else {
-          this.lockedText.update(this.div.innerText);
+          this.lockedText.update(this.div.value);
         }
       }
     });
 
     this.lockedText.addUpdateCallback((text: string) => {
-      this.div.innerHTML = text;
+      const oldOffset = this.div.selectionEnd;
+      this.div.value = text;
+      this.div.selectionStart = oldOffset;
+      this.div.selectionEnd = oldOffset;
     });
   }
+
 }
