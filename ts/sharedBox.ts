6b87bba3178bfd3ad64214e93c479e10a8662f8f
Matt Deeds <mdeeds@gmail.com>
2021-05-09 13:54:22 -0700

    Format on drag-drop

--- ts/sharedBox.ts
+++ ts/sharedBox.ts
@@ -1,5 +1,7 @@
 import { LockedText } from "./lockedText";
 import { PeerGroupInterface } from "./peerGroupInterface";
+import beautify from "js-beautify";
+import { resolve } from "path";
 
 export class SharedBox {
   private lockedText: LockedText;
@@ -16,15 +18,34 @@ export class SharedBox {
     container.appendChild(this.div);
 
     this.div.addEventListener('keyup', async (ev: KeyboardEvent) => {
-      if (this.lastContent != this.div.value) {
-        if (!this.lockedText.hasLock()) {
-          this.div.selectionStart--;
-          this.setText(
-            await this.lockedText.takeLock());
-        } else {
-          this.lockedText.update(this.div.value);
-        }
-      }
+      this.handleUpdate();
+    });
+
+    this.div.addEventListener('drop', async (ev) => {
+      const oldOffset = this.div.selectionStart;
+      await this.handleUpdate();
+      setTimeout(() => {
+        this.div.value = beautify(this.div.value, {
+          "indent_size": 2,
+          "indent_char": " ",
+          "max_preserve_newlines": 1,
+          "preserve_newlines": true,
+          "keep_array_indentation": false,
+          "break_chained_methods": false,
+          "brace_style": "collapse",
+          "space_before_conditional": true,
+          "unescape_strings": false,
+          "jslint_happy": false,
+          "end_with_newline": false,
+          "wrap_line_length": 80,
+          "comma_first": false,
+          "e4x": false,
+          "indent_empty_lines": false
+        });
+        this.div.selectionStart = oldOffset;
+        this.div.selectionEnd = oldOffset;
+        this.handleUpdate();
+      }, 0);
     });
 
     this.lockedText.addUpdateCallback((text: string) => {
@@ -37,5 +58,21 @@ export class SharedBox {
     this.div.value = text;
     this.div.selectionStart = oldOffset;
     this.div.selectionEnd = oldOffset;
+  }
+
+  private async handleUpdate(): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      if (this.lastContent != this.div.value) {
+        if (!this.lockedText.hasLock()) {
+          this.div.selectionStart--;
+          this.setText(
+            await this.lockedText.takeLock());
+          resolve();
+        } else {
+          this.lockedText.update(this.div.value);
+          resolve();
+        }
+      }
+    });
   }
 }
